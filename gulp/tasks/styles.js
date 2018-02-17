a6b3c3421f346c540244a1cc7418bfb4423af641
Anjo Tadena <anjo@coderepubliq.com>
2018-02-17 20:30:54 +0800

    Complete header style.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,6 +5,7 @@ var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function () {
 	gulp.src('./app/styles/styles.css')
@@ -14,6 +15,7 @@ gulp.task('styles', function () {
 				mixins,
 				cssvars,
 				nested,
+				hexrgba,
 				autoprefixer
 			]
 		))
